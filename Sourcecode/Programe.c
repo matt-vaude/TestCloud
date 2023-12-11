b11c3d8b13d53cd7db76c9ae694d7e418c09b0b8
matt-vaude <115538820+matt-vaude@users.noreply.github.com>
2023-12-11 23:27:45 +0100

    Update Programe.c

--- Sourcecode/Programe.c
+++ Sourcecode/Programe.c
@@ -1,13 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-void function_with_leak() {
-    int *ptr = malloc(10 * sizeof(int)); // Allocation dynamique de mémoire
-    // La mémoire allouée n'est jamais libérée
-    // Cette ligne manque : free(ptr);
-}
-
 int main() {
-    function_with_leak();
-    // Le programme se termine sans libérer la mémoire allouée, entraînant une fuite
-    return 0;
+    int data = malloc(10 sizeof(int)); // Allocation de mémoire
+    int i = 0;
+
+    if (data == NULL) {
+        printf("Erreur d'allocation de mémoire\n");
+        exit(1);
+    }
+
+    while (i < 15) { // Dépassement des bornes du tableau
+        data[i] = i;
+        printf("Valeur: %d\n", data[i]);
+        i++;
+    }
+
+    int moreData = malloc(5 sizeof(int)); // Nouvelle allocation sans libération de la précédente
+    if (moreData != NULL) {
+        for (int j = 0; j < 5; j++) {
+            moreData[j] = j * 2;
+        }
+    }
+
+    free(moreData); // Libération de la seconde allocation mais pas de la première
+    // La mémoire allouée à 'data' n'est jamais libérée (fuite de mémoire)
+
+    int x = 10, y = 0;
+    int result = x / y; // Division par zéro
+
+    char *str = "Texte";
+    str[0] = 't'; // Tentative de modification d'une chaîne littérale
+
+    return 0; // Fin du programme sans libérer toute la mémoire allouée
 }
