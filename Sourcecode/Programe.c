05430494bde9c2444d6391c29a66d3e18f32f77c
matt-vaude <115538820+matt-vaude@users.noreply.github.com>
2023-12-11 23:22:32 +0100

    Update Programe.c

--- Sourcecode/Programe.c
+++ Sourcecode/Programe.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
+#include <stdlib.h>
+
+void function_with_leak() {
+    int *ptr = malloc(10 * sizeof(int)); // Allocation dynamique de mémoire
+    // La mémoire allouée n'est jamais libérée
+    // Cette ligne manque : free(ptr);
+}
 
 int main() {
-    printf("Bonjour, monde !\n");
+    function_with_leak();
+    // Le programme se termine sans libérer la mémoire allouée, entraînant une fuite
     return 0;
 }
